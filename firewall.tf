67668ba6b910a3ded301edc18167dd1601d14dde
hgure <hgure83@gmail.com>
2026-01-07 21:40:38 -0500

    add rules

--- firewall.tf
+++ firewall.tf
@@ -13,7 +13,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv6" {
   cidr_ipv4         = "0.0.0.0/0"
   from_port         = 80
   ip_protocol       = "tcp"
-  to_port           = 80
+  to_port           = 100
 }
 
 resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
